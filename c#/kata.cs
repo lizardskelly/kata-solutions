88e527edfe33b87198290530a5e86e3c15635836
Liz Meeker <lizmeeker90@gmail.com>
2022-10-14 13:55:47 -0400

    added c# solution

--- c#/kata.cs
+++ c#/kata.cs
@@ -26,6 +26,29 @@ Notes
   {
     return -Math.Abs(number);
   }
-}
 
 // Best solution: same as mine
+
+// -----------------------
+
+/* Grasshopper - Debug sayHello - 8 kyu
+The starship Enterprise has run into some problem when creating a program to greet everyone as they come aboard. 
+It is your job to fix the code and get the program working again!
+
+Example output:
+
+Hello, Mr. Spock
+*/
+
+// My solution: just interpolate the input. duh again
+
+  public static string SayHello(string name)
+  {
+    return $"Hello, {name}";
+  }
+
+// Best solution: same as mine
+
+// -----------------------
+
+}
