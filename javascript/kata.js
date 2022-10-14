88a2721ee6f52450aa71f963e651ae52b4d78b10
Liz Meeker <lizmeeker90@gmail.com>
2022-10-14 12:48:20 -0400

    forgot a solution title

--- javascript/kata.js
+++ javascript/kata.js
@@ -1,4 +1,4 @@
-/* 6 kyu
+/* Bit Counting - 6 kyu
 Write a function that takes an integer as input, and returns the number of bits that are equal to one in the binary representation of that number. 
 You can guarantee that input is non-negative.
 
