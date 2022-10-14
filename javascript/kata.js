ada5a9ad09b7ef8e70bac5bd31a8d5b939fc16c4
Liz Meeker <lizmeeker90@gmail.com>
2022-10-14 12:07:57 -0400

    added solution

--- javascript/kata.js
+++ javascript/kata.js
@@ -15,3 +15,36 @@ const countBits = input => {
 
 countBits = n => n.toString(2).split('0').join('').length;
 
+// ----------------------
+
+/* Find the divisors! - 7 kyu
+Create a function named divisors/Divisors that takes an integer n > 1 and returns an array with all of the integer's divisors
+(except for 1 and the number itself), from smallest to largest. If the number is prime return the string '(integer) is prime'.
+
+Example:
+
+divisors(12); // should return [2,3,4,6]
+divisors(25); // should return [5]
+divisors(13); // should return "13 is prime"
+*/
+
+// My solution: declare empty array, populate array using for loop that counts up to half the input number 
+// (since you will never have divisors past half an integer), ternary returns correct answer depending on array length
+
+const divisors = integer => {
+	let divisorArray = [];
+  for (let i = 2; i <= integer / 2; i++) {
+  	if (integer % i == 0) {
+    	divisorArray.push(i);
+    };
+  };
+  return divisorArray.length > 0 ? divisorArray : `${integer} is prime`;
+};
+
+// Best solution: same as mine, but condensed
+
+function divisors(integer) {
+  var res = []
+  for (var i = 2; i <= Math.floor(integer / 2); ++i) if (integer % i == 0) res.push(i);
+  return res.length ? res : integer + ' is prime'
+};
