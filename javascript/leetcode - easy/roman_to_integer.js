481e52accd6a999df0d3e3fcb0f718cdacb62568
Liz Meeker <lizmeeker90@gmail.com>
2022-10-25 13:07:08 -0400

    added new js solution

---
+++ javascript/leetcode - easy/roman_to_integer.js
@@ -0,0 +1,75 @@
+/* Roman to Integer
+Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
+
+Symbol       Value
+I             1
+V             5
+X             10
+L             50
+C             100
+D             500
+M             1000
+
+For example, 2 is written as II in Roman numeral, just two ones added together. 12 is written as XII, which is simply X + II. 
+The number 27 is written as XXVII, which is XX + V + II.
+
+Roman numerals are usually written largest to smallest from left to right. However, the numeral for four is not IIII. 
+Instead, the number four is written as IV. Because the one is before the five we subtract it making four. 
+The same principle applies to the number nine, which is written as IX. There are six instances where subtraction is used:
+
+    I can be placed before V (5) and X (10) to make 4 and 9. 
+    X can be placed before L (50) and C (100) to make 40 and 90. 
+    C can be placed before D (500) and M (1000) to make 400 and 900.
+
+Given a roman numeral, convert it to an integer.
+
+
+Example 1:
+
+Input: s = "III"
+Output: 3
+Explanation: III = 3.
+
+Example 2:
+
+Input: s = "LVIII"
+Output: 58
+Explanation: L = 50, V= 5, III = 3.
+
+Example 3:
+
+Input: s = "MCMXCIV"
+Output: 1994
+Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
+
+ 
+
+Constraints:
+
+    1 <= s.length <= 15
+    s contains only the characters ('I', 'V', 'X', 'L', 'C', 'D', 'M').
+    It is guaranteed that s is a valid roman numeral in the range [1, 3999].
+*/
+
+// My solution: declare a values object with keys/properties set to roman numeral values. create an array that splits the roman numerals and reverse it.
+// return a reduce function on the numerals array that add or subtracts the next numeral in the array depending on if it is larger or smaller
+// than the total sum
+
+const romanToInt = numeralString => {
+	const values = {
+  	I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000
+  };
+	let numerals = numeralString.split('').reverse();
+  return numerals.reduce((sum, numeral, index) => {
+  	const value = values[numeral];
+  	return (sum <= value || numeral === numerals[index - 1]) ?
+    	sum + value :
+    	sum - value;
+  }, 0);
+}
